881d3e66feb030617505728ee8037e4e28af3cf0
Iannis Poquet <ianpo@live.fr>
2024-01-13 19:26:06 +0100

    Add test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+from ctypes import cdll
+
+def find_files(filename, search_path):
+   result = []
+
+# Wlaking top-down from the root
+   for root, dir, files in os.walk(search_path):
+      if filename in files:
+         result.append(os.path.join(root, filename))
+   return result
+
+dlls = find_files("MLCore.dll", "./")
+dllPath = dlls[0]
+print("Using '" + dllPath + "'.")
+libc = cdll.LoadLibrary(dllPath)
+print("X = " + str(libc.infos()))
