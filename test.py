4013b9117266ff03ec6f103920556c5b0bd580c9
Eliot Froment <fromenteliot@gmail.com>
2024-01-14 12:48:30 +0100

    All graph funcitons (3d graphs are broken)

--- test.py
+++ test.py
@@ -1,28 +1,247 @@
 import os
+
+import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import numpy as np
+
 from ctypes import cdll
 
 
 def find_files(filename, search_path):
     result = []
 
-    # Wlaking top-down from the root
+    # Walking top-down from the root
     for root, dir, files in os.walk(search_path):
         if filename in files:
             result.append(os.path.join(root, filename))
     return result
 
+
 def find_dll():
-   dlls = find_files("MLCore.dll", "./")
-   if (len(dlls) == 0):
-       dlls = find_files("libMLCore.dll", "./")
-   return dlls
+    dlls = find_files("MLCore.dll", "./")
+    if (len(dlls) == 0):
+        dlls = find_files("libMLCore.dll", "./")
+    return dlls
+
+
+# Plotting functions
+# Classification
+
+def LinearSimple():
+    X = np.array([
+        [1, 1],
+        [2, 3],
+        [3, 3]
+    ])
+
+    plt.scatter(X[0, 0], X[0, 1], color='blue')
+    plt.scatter(X[1:3, 0], X[1:3, 1], color='red')
+    plt.title = 'Linear Simple'
+    plt.show()
+    plt.clf()
+
+
+def LinearMultiple():
+    X = np.concatenate(
+        [np.random.random((50, 2)) * 0.9 + np.array([1, 1]), np.random.random((50, 2)) * 0.9 + np.array([2, 2])])
+
+    plt.scatter(X[0:50, 0], X[0:50, 1], color='blue')
+    plt.scatter(X[50:100, 0], X[50:100, 1], color='red')
+    plt.show()
+    plt.clf()
+
+
+def XOR():
+    X = np.array([[1, 0], [0, 1], [0, 0], [1, 1]])
+
+    plt.scatter(X[0:2, 0], X[0:2, 1], color='blue')
+    plt.scatter(X[2:4, 0], X[2:4, 1], color='red')
+    plt.show()
+    plt.clf()
+
+
+def Cross():
+    X = np.random.random((500, 2)) * 2.0 - 1.0
+    Y = np.array([1 if abs(p[0]) <= 0.3 or abs(p[1]) <= 0.3 else -1 for p in X])
+
+    plt.scatter(np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]] == 1, enumerate(X)))))[:, 0],
+                np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]] == 1, enumerate(X)))))[:, 1],
+                color='blue')
+    plt.scatter(np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]] == -1, enumerate(X)))))[:, 0],
+                np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]] == -1, enumerate(X)))))[:, 1],
+                color='red')
+    plt.show()
+    plt.clf()
+
+
+def MultiLinear3Classes():
+    X = np.random.random((500, 2)) * 2.0 - 1.0
+    Y = np.array([[1, 0, 0] if -p[0] - p[1] - 0.5 > 0 > p[1] and p[0] - p[1] - 0.5 < 0 else
+                  [0, 1, 0] if -p[0] - p[1] - 0.5 < 0 < p[1] and p[0] - p[1] - 0.5 < 0 else
+                  [0, 0, 1] if -p[0] - p[1] - 0.5 < 0 < p[0] - p[1] - 0.5 and p[1] < 0 else
+                  [0, 0, 0] for p in X])
+
+    X = X[[not np.all(arr == [0, 0, 0]) for arr in Y]]
+    Y = Y[[not np.all(arr == [0, 0, 0]) for arr in Y]]
+
+    plt.scatter(np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][0] == 1, enumerate(X)))))[:, 0],
+                np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][0] == 1, enumerate(X)))))[:, 1],
+                color='blue')
+    plt.scatter(np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][1] == 1, enumerate(X)))))[:, 0],
+                np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][1] == 1, enumerate(X)))))[:, 1],
+                color='red')
+    plt.scatter(np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][2] == 1, enumerate(X)))))[:, 0],
+                np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][2] == 1, enumerate(X)))))[:, 1],
+                color='green')
+    plt.show()
+    plt.clf()
+
+
+def MultiCross():
+    X = np.random.random((1000, 2)) * 2.0 - 1.0
+    Y = np.array([[1, 0, 0] if abs(p[0] % 0.5) <= 0.25 and abs(p[1] % 0.5) > 0.25 else [0, 1, 0] if abs(
+        p[0] % 0.5) > 0.25 and abs(p[1] % 0.5) <= 0.25 else [0, 0, 1] for p in X])
+
+    plt.scatter(np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][0] == 1, enumerate(X)))))[:, 0],
+                np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][0] == 1, enumerate(X)))))[:, 1],
+                color='blue')
+    plt.scatter(np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][1] == 1, enumerate(X)))))[:, 0],
+                np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][1] == 1, enumerate(X)))))[:, 1],
+                color='red')
+    plt.scatter(np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][2] == 1, enumerate(X)))))[:, 0],
+                np.array(list(map(lambda elt: elt[1], filter(lambda c: Y[c[0]][2] == 1, enumerate(X)))))[:, 1],
+                color='green')
+    plt.show()
+    plt.clf()
+
+
+# Regression
+
+def LinearSimple2D():
+    X = np.array([
+        [1],
+        [2]
+    ])
+    Y = np.array([
+        2,
+        3
+    ])
+
+    plt.scatter(X, Y)
+    plt.show()
+    plt.clf()
+
+
+def NonLinearSimple2D():
+    X = np.array([
+        [1],
+        [2],
+        [3]
+    ])
+    Y = np.array([
+        2,
+        3,
+        2.5
+    ])
+
+    plt.scatter(X, Y)
+    plt.show()
+    plt.clf()
+
+
+def LinearSimple3D():
+    X = np.array([
+        [1, 1],
+        [2, 2],
+        [3, 1]
+    ])
+    Y = np.array([
+        2,
+        3,
+        2.5
+    ])
+
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(X[:, 0], X[:, 1], Y)
+    plt.show()
+    plt.clf()
+
+
+def LinearTricky3D():
+    X = np.array([
+        [1, 1],
+        [2, 2],
+        [3, 3]
+    ])
+    Y = np.array([
+        1,
+        2,
+        3
+    ])
+
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(X[:, 0], X[:, 1], Y)
+    plt.show()
+    plt.clf()
+
+
+def NonLinearSimple3D():
+    X = np.array([
+        [1, 0],
+        [0, 1],
+        [1, 1],
+        [0, 0],
+    ])
+    Y = np.array([
+        2,
+        1,
+        -2,
+        -1
+    ])
+
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(X[:, 0], X[:, 1], Y)
+    plt.show()
+    plt.clf()
+
+
+def AllGraphs():
+    # Classification
+    LinearSimple()
+    LinearMultiple()
+    XOR()
+    Cross()
+    MultiLinear3Classes()
+    MultiCross()
+
+    # Regression
+    LinearSimple2D()
+    NonLinearSimple2D()
+    LinearSimple3D()
+    LinearTricky3D()
+    NonLinearSimple3D()
+
+
+# Main
 
 dlls = find_dll()
 if len(dlls) == 0:
-   print("didn't found dll named 'MLCore.dll' nor 'libMLCore.dll'.")
-   exit()
+    print("didn't found dll named 'MLCore.dll' nor 'libMLCore.dll'.")
+    exit()
 
 dllPath = dlls[0]
 print("Using '" + dllPath + "'.")
 libc = cdll.LoadLibrary(dllPath)
 print("X = " + str(libc.infos()))
+
+libc.initialize()
+
+AllGraphs()
+
+libc.shutdown()
